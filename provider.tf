a6d0483db7d39caa4afef3916a3a44b94978e02e
csm123455 <csm123455@gmail.com>
2025-06-25 13:12:12 +0900

    fix *.tf

--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-	  bucket         = "backend-bucket-csm"
-	  key            = "terraform/state-test/terraform.tfstate"
-	  region         = "ap-northeast-2"
+    bucket         = "backend-bucket-csm"
+    key            = "terraform/state-test/terraform.tfstate"
+    region         = "ap-northeast-2"
     dynamodb_table = "DynamoDB-Lock"
-	}
+  }
 }
 
 provider "aws" {
